b7d648255172f790268c6633afea5b7a1d2a43ee
aboutaha12 <tahabalkhi@hotmail.com>
2024-02-23 16:51:09 +0100

    first try with fqi

--- src/train.py
+++ src/train.py
@@ -1,5 +1,11 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+
+import numpy as np
+from tqdm import tqdm
+
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -7,15 +13,89 @@ env = TimeLimit(
 # Now is the floor is yours to implement the agent and train it.
 
 
+def collect_samples(env, horizon, disable_tqdm=False, print_done_states=False):
+    s, _ = env.reset()
+    # dataset = []
+    S = []
+    A = []
+    R = []
+    S2 = []
+    D = []
+    for _ in tqdm(range(horizon), disable=disable_tqdm):
+        a = env.action_space.sample()
+        s2, r, done, trunc, _ = env.step(a)
+        # dataset.append((s,a,r,s2,done,trunc))
+        S.append(s)
+        A.append(a)
+        R.append(r)
+        S2.append(s2)
+        D.append(done)
+        if done or trunc:
+            s, _ = env.reset()
+            if done and print_done_states:
+                print("done!")
+        else:
+            s = s2
+    S = np.array(S)
+    A = np.array(A).reshape((-1, 1))
+    R = np.array(R)
+    S2 = np.array(S2)
+    D = np.array(D)
+    return S, A, R, S2, D
+
+
+def rf_fqi(S, A, R, S2, D, iterations, nb_actions, gamma, disable_tqdm=False):
+    nb_samples = S.shape[0]
+    Qfunctions = []
+    SA = np.append(S, A, axis=1)
+    for iter in tqdm(range(iterations), disable=disable_tqdm):
+        if iter == 0:
+            value = R.copy()
+        else:
+            Q2 = np.zeros((nb_samples, nb_actions))
+            for a2 in range(nb_actions):
+                A2 = a2 * np.ones((S.shape[0], 1))
+                S2A2 = np.append(S2, A2, axis=1)
+                Q2[:, a2] = Qfunctions[-1].predict(S2A2)
+            max_Q2 = np.max(Q2, axis=1)
+            value = R + gamma * (1 - D) * max_Q2
+        Q = RandomForestRegressor()
+        Q.fit(SA, value)
+        Qfunctions.append(Q)
+    return Qfunctions
+
+
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 class ProjectAgent:
+    def __init__(self, Qvalue=None):
+        self.nactions = env.action_space.n
+        self.Qvalue = Qvalue
+
     def act(self, observation, use_random=False):
-        return 0
+        # Act greedily
+        Qsa = []
+        for a in range(self.nactions):
+            sa = np.append(observation, a).reshape(1, -1)
+            Qsa.append(self.Qvalue.predict(sa))
+        return np.argmax(Qsa)
 
     def save(self, path):
-        pass
+        with open(path, "wb") as model:
+            pickle.dump(self.Qvalue, model)
 
     def load(self):
-        pass
+        with open("src/model.pkl", "rb") as model:
+            self.Qvalue = pickle.load(model)
+
+
+if __name__ == "__main__":
+    gamma = 0.9
+    nb_iter = 10
+    nb_actions = env.action_space.n
+    S, A, R, S2, D = collect_samples(env, int(1e3))
+    Qfunctions = rf_fqi(S, A, R, S2, D, nb_iter, nb_actions, gamma)
+    Qvalue = Qfunctions[-1]
+    agent = ProjectAgent(Qvalue)
+    agent.save("src/model.pkl")
