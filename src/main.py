0cc8d2c830c93c523de66f62f4803d9fe3397dda
aboutaha12 <tahabalkhi@hotmail.com>
2024-02-24 18:39:50 +0100

    DQN with 5 gs

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ import numpy as np
 import torch
 
 from evaluate import evaluate_HIV, evaluate_HIV_population
-from train import ProjectAgent  # Replace DummyAgent with your agent implementation
+from train_dqn import (
+    ProjectAgentDQN,
+    config,
+)  # Replace DummyAgent with your agent implementation
+from train import ProjectAgent
 
 
 def seed_everything(seed: int = 42):
@@ -21,10 +25,12 @@ def seed_everything(seed: int = 42):
 if __name__ == "__main__":
     seed_everything(seed=42)
     # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
-    agent = ProjectAgent()
+    # agent = ProjectAgent()
+    agent = ProjectAgentDQN(config)
     agent.load()
     # Keep the following lines to evaluate your agent unchanged.
     score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
+    print("Agent score : ", score_agent)
     score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=15)
     with open(file="score.txt", mode="w") as f:
         f.write(f"{score_agent}\n{score_agent_dr}")
